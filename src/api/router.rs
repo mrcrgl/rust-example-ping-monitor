82374cce03837b9759c3b23ba446a705264c5f44
Marc Riegel <mail@mrcrgl.de>
2025-04-06 21:47:35 +0200

    added some unit and integration tests and ci

--- src/api/router.rs
+++ src/api/router.rs
@@ -1,26 +1,27 @@
-use crate::persistence::{SharedStateTargetDatabase, Target, TargetId};
+use crate::persistence::{SharedStateTargetDatabase, Target, TargetId, TargetProbeResult};
+use axum::extract::Path;
 use axum::http::StatusCode;
-use axum::routing::{get, post};
+use axum::routing::get;
 use axum::{Extension, Json};
+use std::collections::VecDeque;
 use std::net::IpAddr;
 use std::sync::Arc;
 
 pub(super) fn setup_router() -> axum::Router {
-    let router = axum::Router::new()
-        .route("/targets", get(get_targets))
-        .route("/targets", post(add_target));
-
-    router
+    axum::Router::new()
+        .route("/targets", get(list_targets).post(add_target))
+        .route("/targets/{:id}", get(get_target).delete(delete_target))
+        .route("/targets/{:id}/results", get(get_target_results))
 }
 
-async fn get_targets(
+async fn list_targets(
     Extension(database): Extension<Arc<SharedStateTargetDatabase>>,
 ) -> Json<Vec<Target>> {
     let mut targets = vec![];
 
     for key in database.list_keys().await {
         if let Some(target) = database.get(&key).await {
-            targets.push(target.clone());
+            targets.push(target.target.clone());
         }
     }
 
@@ -44,4 +45,31 @@ async fn add_target(
     database.insert(target.clone()).await;
 
     Ok(Json(target))
+}
+
+async fn delete_target(
+    Extension(database): Extension<Arc<SharedStateTargetDatabase>>,
+    Path(id): Path<TargetId>,
+) -> StatusCode {
+    let _ = database.delete(id).await;
+
+    StatusCode::ACCEPTED
+}
+
+async fn get_target(
+    Extension(database): Extension<Arc<SharedStateTargetDatabase>>,
+    Path(id): Path<TargetId>,
+) -> axum::response::Result<Json<Target>> {
+    let entry = database.get(&id).await.ok_or(StatusCode::NOT_FOUND)?;
+
+    Ok(Json(entry.target))
+}
+
+async fn get_target_results(
+    Extension(database): Extension<Arc<SharedStateTargetDatabase>>,
+    Path(id): Path<TargetId>,
+) -> axum::response::Result<Json<VecDeque<TargetProbeResult>>> {
+    let entry = database.get(&id).await.ok_or(StatusCode::NOT_FOUND)?;
+
+    Ok(Json(entry.probe_results))
 }
--- src/persistence/models.rs
+++ src/persistence/models.rs
@@ -1,10 +1,33 @@
+use std::collections::VecDeque;
 use std::net::IpAddr;
 use uuid::Uuid;
 
 pub type TargetId = Uuid;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct TargetTableEntry {
+    pub target: Target,
+    pub probe_results: VecDeque<TargetProbeResult>,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Target {
     pub id: TargetId,
     pub address: IpAddr,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct TargetProbeResult {
+    pub probe_time: chrono::DateTime<chrono::Utc>,
+    pub took: i64,
+    #[serde(flatten)]
+    pub result: ProbeStatus,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "status", rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ProbeStatus {
+    Ok { rtt: i64 },
+    Timeout,
+    Failure { reason: String },
 }
